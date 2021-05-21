9b984fa7f2ccfc47ad88b15b0ccd1d59a808194b
fakynno <fakynno@gmail.com>
2021-05-20 23:45:48 -0300

    adicionada lib do postgre e criado singleton de conexao com o db

---
+++ src/main/java/connection/SingleConnection.java
@@ -0,0 +1,41 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Responsável por fazer a conexão com o banco de dados
+ * @author Fakyn
+ *
+ */
+public class SingleConnection {
+
+	private static String banco = "jdbc:postgresql//localhost:5432/curso-jsp?autoReconnect=true";
+	private static String password = "124578";
+	private static String user = "postgres";
+	private static Connection connection = null;
+	
+	static {
+		conectar();
+	}
+	
+	public SingleConnection() {
+		conectar();
+	}
+	
+	private static void conectar() {
+		try {
+			if(connection == null) {
+				Class.forName("org.postgresql.Driver");
+				connection = DriverManager.getConnection(banco, user, password);
+				connection.setAutoCommit(false);
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Erro ao conectar com o banco de dados");
+		}
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+}
