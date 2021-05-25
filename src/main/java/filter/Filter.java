b26d8d48f7dbda522810768c1dfedbed296a82bb
fakynno <fakynno@gmail.com>
2021-05-24 23:50:30 -0300

    Ajustes na classe Filter

--- src/main/java/filter/Filter.java
+++ src/main/java/filter/Filter.java
@@ -1,28 +1,35 @@
 package filter;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 
+import connection.SingleConnection;
+
+@WebFilter(urlPatterns = { "/*" })
 public class Filter implements javax.servlet.Filter {
 
+	private static Connection connection = SingleConnection.getConnection();
+	
 	@Override
 	public void destroy() {	
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		
+			throws IOException, ServletException {		
+		chain.doFilter(request, response);
 	}
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		
+		connection = SingleConnection.getConnection();
 	}
 
 }
