de11fa0f3af688073550ab4d437c33196eeeb7da
fakynno <fakynno@gmail.com>
2021-05-26 09:23:40 -0300

    Ajuste no path do banco de dados

--- src/main/java/connection/SingleConnection.java
+++ src/main/java/connection/SingleConnection.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
  */
 public class SingleConnection {
 
-	private static String banco = "jdbc:postgresql//localhost:5432/curso-jsp?autoReconnect=true";
+	private static String banco = "jdbc:postgresql://localhost:5432/curso-jsp?autoReconnect=true";
 	private static String password = "124578";
 	private static String user = "postgres";
 	private static Connection connection = null;
@@ -31,7 +31,7 @@ public class SingleConnection {
 				connection.setAutoCommit(false);
 			}
 		} catch (Exception e) {
-			e.printStackTrace();;
+			e.printStackTrace();
 			throw new RuntimeException("Erro ao conectar com o banco de dados");
 		}
 	}
