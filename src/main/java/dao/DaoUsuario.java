8e7c0500b9a31b3347a82029c0edf7c94c6b13fd
fakynno <fakynno@gmail.com>
2021-06-13 01:03:04 -0300

    Add metodo de exclusao no DaoLogin

--- src/main/java/dao/DaoUsuario.java
+++ src/main/java/dao/DaoUsuario.java
@@ -11,17 +11,17 @@ import beans.BeanCursoJsp;
 import connection.SingleConnection;
 
 public class DaoUsuario {
-	
+
 	private Connection connection;
-	
+
 	public DaoUsuario() {
 		connection = SingleConnection.getConnection();
 	}
-	
+
 	public void salvar(BeanCursoJsp usuario) {
-		
+
 		try {
-			
+
 			String sql = "insert into usuario(login, senha) values (?, ?)";
 			PreparedStatement stmt = connection.prepareStatement(sql);
 			stmt.setString(1, usuario.getLogin());
@@ -37,30 +37,50 @@ public class DaoUsuario {
 				e1.printStackTrace();
 			}
 		}
-		
+
 	}
-	
+
 	public List<BeanCursoJsp> listarUsuarios() throws Exception {
-		
+
 		List<BeanCursoJsp> lista = new ArrayList<BeanCursoJsp>();
-		
+
 		String sql = "select * from usuario";
-		
+
 		PreparedStatement stmt = connection.prepareStatement(sql);
 		ResultSet resultSet = stmt.executeQuery();
-		
+
 		while (resultSet.next()) {
-			
+
 			BeanCursoJsp cursoJsp = new BeanCursoJsp();
-			
+
 			cursoJsp.setLogin(resultSet.getString("login"));
 			cursoJsp.setSenha(resultSet.getString("senha"));
-			
+
 			lista.add(cursoJsp);
 		}
-		
+
 		return lista;
+
+	}
+	
+	public void delete(String login) {
 		
+		try {
+			String usuarioExcluido = login;
+			String sql = "delete from usuario where login = '" + login + "'";
+			
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.execute();
+			connection.commit();
+			System.out.println("Usuário " + usuarioExcluido + " excluído com sucesso");
+		} catch (Exception e) {
+			e.printStackTrace();
+			try {
+				connection.rollback();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
 	}
 
 }
